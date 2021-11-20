314e73d1b8501caa69ff758fc4d92abfc67763df
Prashant Birajdar <92636189+Superprashant@users.noreply.github.com>
2021-11-20 17:34:14 +0530

    Add files via upload

---
+++ 2 number addition.cpp
@@ -0,0 +1,10 @@
+/*write program in C++ to print the sum of two numbers*/
+#include <iostream>
+using namespace std;
+int main()
+{
+int a=29, b=30;
+cout << " The sum of 29 and 30 is :";
+cout << a+b;
+return 0;
+}
